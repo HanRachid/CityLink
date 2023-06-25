4ad5f6c43a93e3ca8a8b9b17e0579af8a8f77ac2
HanRachid <handaouirachid@gmail.com>
2023-06-25 20:55:11 +0100

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -10,37 +10,80 @@ import {
   Navbar,
   Container,
   Image,
+  Card,
+  Form,
 } from 'react-bootstrap';
 import logo from './Pub/Media/CityLinkLogo.png';
 function App() {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    fetch('http://127.0.0.1:3000/posts')
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-
-        setPosts(data);
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-  }, []);
-
   return (
-    <div>
-      <h1>Posts</h1>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>
-            <h2>{post.title}</h2>
-            <p>{post.content}</p>
-            <p>{post.location}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Navbar data-bs-theme='dark' className='bg-body-tertiary'>
+      <Container>
+        <Navbar.Brand href='#home'>CityLink</Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='ms-auto'>
+            <NavDropdown title='Signup' id='basic-nav-dropdown'>
+              <Card style={{width: '22rem'}}>
+                <Card.Body>
+                  <Card.Title>Signup</Card.Title>
+
+                  <Form>
+                    <Form.Group className='mb-3' controlId='formBasicEmail'>
+                      <Form.Label>Email address</Form.Label>
+                      <Form.Control type='email' placeholder='Enter email' />
+                      <Form.Text className='text-muted'>
+                        We'll never share your email with anyone else.
+                      </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group className='mb-3' controlId='formBasicPassword'>
+                      <Form.Label>Password</Form.Label>
+                      <Form.Control type='password' placeholder='Password' />
+                    </Form.Group>
+                    <Form.Group
+                      className='mb-3'
+                      controlId='formBasicCheckbox'
+                    ></Form.Group>
+                    <Button variant='primary' type='submit'>
+                      Submit
+                    </Button>
+                  </Form>
+                </Card.Body>
+              </Card>
+            </NavDropdown>
+            <NavDropdown title='Sign in' id='basic-nav-dropdown'>
+              <Card style={{width: '22rem'}}>
+                <Card.Body>
+                  <Card.Title>Sign in</Card.Title>
+
+                  <Form>
+                    <Form.Group className='mb-3' controlId='formBasicEmail'>
+                      <Form.Label>Email address</Form.Label>
+                      <Form.Control type='email' placeholder='Enter email' />
+                      <Form.Text className='text-muted'>
+                        We'll never share your email with anyone else.
+                      </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group className='mb-3' controlId='formBasicPassword'>
+                      <Form.Label>Password</Form.Label>
+                      <Form.Control type='password' placeholder='Password' />
+                    </Form.Group>
+                    <Form.Group
+                      className='mb-3'
+                      controlId='formBasicCheckbox'
+                    ></Form.Group>
+                    <Button variant='primary' type='submit'>
+                      Submit
+                    </Button>
+                  </Form>
+                </Card.Body>
+              </Card>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
